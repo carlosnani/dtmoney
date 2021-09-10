d70ac2d6aa25d4d7440b6977ec7e6bec8c933ff0
Crlos Nani <carlosnani80@gmail.com>
2021-09-10 19:02:03 -0300

    Added new fotn weight and dashboard / summary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./styles/global";
-import { Header } from "./Header/index";
+import { Header } from "./components/Header/index";
+import { Dashboard } from "./components/Dashboard";
  
 export function App() {
   return (
     <>
       <Header/> 
+      <Dashboard/>
       <GlobalStyle />
     </>
   );
