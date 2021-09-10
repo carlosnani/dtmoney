86ae67b9d7c281d3890b183b110812e6c7c893fc
Crlos Nani <carlosnani80@gmail.com>
2021-09-10 19:38:55 -0300

    Added TransactionsTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./style";
+
+
+export function TransactionsTable() {
+  return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Site</td>
+                        <td className="deposit">R$ 10.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/10/2020</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">R$ -2.000,00</td>
+                        <td>Despesa</td>
+                        <td>20/10/2020</td>
+                    </tr>
+ 
+                </tbody>
+            </table>
+        </Container>    
+  );
+}
