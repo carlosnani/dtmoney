acd418833de54b8778526b3f33047b954987edb9
Crlos Nani <carlosnani80@gmail.com>
2021-09-11 20:22:05 -0300

    Added miragejs to make a server

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-
+import { createServer } from 'miragejs';
  
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => [
+      { id: 1, 
+        title: 'App',
+        amount: 1000,
+        type: 'deposit',
+        category: 'Food',
+        date: new Date()
+      },
+      { id: 2, 
+        title: 'Aluguel',
+        amount: 400,
+        type: 'deposit',
+        category: 'Food',
+        date: new Date()
+      },
+    ]);
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
