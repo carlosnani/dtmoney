b6762cb10a653f9cea7f941b2fa6693e5b0f3e01
Crlos Nani <carlosnani80@gmail.com>
2021-09-11 21:44:37 -0300

    Added Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header/index";
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal';
+import { useState } from "react";
+
+Modal.setAppElement('#root');
  
 export function App() {
+  const [isNewTransition, setIsNewTransition] = useState(false);
+
+  function handleOpenTransitionModal() {
+    setIsNewTransition(true);
+  }
+
+  function handleCloseTransitionModal() {
+    setIsNewTransition(false);
+  }
   return (
     <>
-      <Header/> 
+      <Header onOpenNewTransitionModal={handleOpenTransitionModal}/> 
       <Dashboard/>
+      <Modal 
+        isOpen={isNewTransition}
+        onRequestClose={handleCloseTransitionModal}              
+        >
+            <h2>Nova transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
 import logoImg from '../../assets/logo.svg'; 
 import { Container, Content} from './styles';
- 
 
-export function Header() {
-  const [showModal, setModal] = useState(false);
-  
+interface HeaderProps{
+  onOpenNewTransitionModal: () => void;
+}
+
+export function Header({onOpenNewTransitionModal} : HeaderProps){
 
   return (
     <Container>
         <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">
+            <button type="button" onClick={onOpenNewTransitionModal}>
                 Nova transação    
             </button> 
+ 
         </Content>
     </Container>
   );
